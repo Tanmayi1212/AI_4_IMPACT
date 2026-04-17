8d9080a7c22a393564ef3167ddae48d85e67b58c
unknown <abhinav.sadineni@gmail.com>
2026-04-17 11:22:27 +0530

    fix(deploy): allow problem-statements static export

--- src/app/problem-statements/page.jsx
+++ src/app/problem-statements/page.jsx
@@ -1,8 +1,6 @@
 import ProblemStatementsPublicView from "../../components/problem-statements/ProblemStatementsPublicView";
 import { getProblemStatementCatalog } from "../../../lib/server/problem-statements";
 
-export const dynamic = "force-dynamic";
-
 export default function ProblemStatementsPage() {
   const statements = getProblemStatementCatalog();
 
